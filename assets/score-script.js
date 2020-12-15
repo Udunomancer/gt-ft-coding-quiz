54ad73136041a4c570ebbe9ce32091154f0e4a1d
Udunomancer <kyle717@gmail.com>
2020-12-15 18:35:08 -0500

    Added js functionality to write out high scores in score.html

--- assets/score-script.js
+++ assets/score-script.js
@@ -1,4 +1,8 @@
 var scorePane = document.getElementById("score-pane");
 var highScores = JSON.parse(localStorage.getItem("storedScoresLS"));
 
-console.log(highScores);
+for (var i = 0; i < highScores.length; i++) {
+    var scoreEl = document.createElement("p");
+    scoreEl.textContent = (i + 1) + ". " + highScores[i].initials + " - " + highScores[i].score;
+    scorePane.appendChild(scoreEl);
+}
