1b0ecda07430e69c07712ed2b319af525ad6cd45
Udunomancer <kyle717@gmail.com>
2020-12-15 20:35:51 -0500

    commented purpose of functions in score-script.js

--- assets/score-script.js
+++ assets/score-script.js
@@ -1,33 +1,39 @@
-  // ===DOM VARIABLES===
-  var scorePane = document.getElementById("score-pane");
-  var clearBtn = document.getElementById("clear-btn");
+// ===DOM VARIABLES===
+var scorePane = document.getElementById("score-pane");
+var clearBtn = document.getElementById("clear-btn");
 
-  // ===JAVASCRIPT VARIABLES===
-  var highScores = JSON.parse(localStorage.getItem("storedScoresLS"));
-  
-  // ===FUNCTION DEFINITIONS===
-  function init() {
-    for (var i = 0; i < highScores.length; i++) {
-        var scoreEl = document.createElement("p");
-        scoreEl.textContent =
-        i + 1 + ". " + highScores[i].initials + " - " + highScores[i].score;
-        scoreEl.setAttribute("class", "text-white rounded w-25 p-1");
-        scoreEl.setAttribute("style", "background-color: #67ace7")
-        scorePane.appendChild(scoreEl);
-    }
+// ===JAVASCRIPT VARIABLES===
+var highScores = JSON.parse(localStorage.getItem("storedScoresLS"));
+
+// ===FUNCTION DEFINITIONS===
+function init() {
+  //Function to run on page load to display high scores
+  //Input: none
+  //Output: none
+  for (var i = 0; i < highScores.length; i++) {
+    var scoreEl = document.createElement("p");
+    scoreEl.textContent =
+      i + 1 + ". " + highScores[i].initials + " - " + highScores[i].score;
+    scoreEl.setAttribute("class", "text-white rounded w-25 p-1");
+    scoreEl.setAttribute("style", "background-color: #67ace7");
+    scorePane.appendChild(scoreEl);
   }
+}
 
-  function clearHighScores() {
-      highScores = [];
-      localStorage.removeItem("storedScoresLS");
-      scorePane.textContent = "";
-  }
+function clearHighScores() {
+  //Function to clear high scores from JS Variables and local storage and clear currently displayed scores from page
+  //Input: none
+  //Output: none
+  highScores = [];
+  localStorage.removeItem("storedScoresLS");
+  scorePane.textContent = "";
+}
 
-  // ===FUNCTION CALLS===
-  init();
+// ===FUNCTION CALLS===
+init();
 
-  // ===EVENT LISTENERS===
-  clearBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    clearHighScores();
-  });
+// ===EVENT LISTENERS===
+clearBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  clearHighScores();
+});
